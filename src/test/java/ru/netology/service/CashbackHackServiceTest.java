5be337ee2be92bce8dd1b713fee849d7517c5254
Елена <koe090590@gmail.com>
2023-10-15 01:28:23 +0300

    3 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,18 @@
-package ru.netology.service;
+import ru.netology.service.CashbackHackService;
+
+import static org.testng.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    void Cashback() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
 }
